e6f7e191fed577102667c083de169db5e4d48aa3
mo.ibrahh <108186067+moibrahh@users.noreply.github.com>
2024-09-19 14:41:13 +0000

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+import asyncio
+from dotenv import load_dotenv
+from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, cli, llm
+from livekit.agents.voice_assistant import VoiceAssistant
+from livekit.plugins import openai, silero
+
+
+load_dotenv()
+
+
+async def entrypoint(ctx: JobContext):
+    pass
+
+if __name__ == "__main__":
+    cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint))
